1da2120c96b38707e32ece5b55b28897b2561261
Linda <lasain@utu.fi>
2025-06-24 06:12:19 +0000

    cleaning

--- scripts/catchment_attributes/catchment_mean_climate.py
+++ scripts/catchment_attributes/catchment_mean_climate.py
@@ -19,13 +19,12 @@ def mean_weather(args):
     weather.index.name = 'date'
 
     with rioxr.open_rasterio(src_path, mask_and_scale=True) as data_array:
-        # iterating over the days in the file 
+        # Iterating over the days in the file 
         for time_step in data_array.Time:
             time = time_step.item()
             one_day_data = data_array.sel({'Time':time})
 
             row = []
-            # looks a bit unpythonic but DataFrame.iterrows returns series, not dataframes and it's a hassle to change back
             for i in range(len(watersheds)): 
                 watershed = watersheds.iloc[[i]]
                 place_id = watershed.Paikka_Id[i]
@@ -34,26 +33,26 @@ def mean_weather(args):
                 clipped = one_day_data.rio.clip(watershed.geometry.values, crs=watershed.crs)
                 average = clipped.mean().item()
                 average = round(average, 1)
+                
                 # Failsafe for catchments smaller than the pixel size
                 if average is np.nan:
                     clipped = data_array.rio.clip(watershed.geometry.values, crs=watershed.crs, all_touched=True)
                     average = clipped.mean().item()
 
                 row.append(average)
+                
             weather.loc[str(time)] = row
             
     weather.to_csv(dst_path)
 
+# Root directories for data
 root = os.getcwd()
-
 dst_root = os.path.join(root, "raw_time_series")
-
 src_root = os.path.join(root, "fmi_grid")
 
+# Path to the vector watershed file
 vector_watershed_path = "CAMELS-FI_catchments.gpkg"
 watersheds = geopd.read_file(vector_watershed_path, layer='v1')
-
-
 
 dirs = ['RRday', 'ET0_FAO', 'Tday',
         'Tgmin', 'Tmin', 'Tmax',
@@ -63,10 +62,9 @@ attributes = {'Rh' : 'humidity' ,'ET0_FAO': 'pet', 'Tday': 'temperature_mean',
               'Tmin': 'temperature_min', 'Tgmin': 'temperature_gmin', 'Tmax': 'temperature_max',
               'RRday': 'precipitation', 'Globrad': 'radiation_global', 'Snow': 'snow_depth'}
 
-
-args = []
 years = (1961, 2023)
 
+args = []
 for current_dir in dirs:
     dst_dir =  os.path.join(dst_root, attributes[current_dir])
     if not os.path.exists(dst_dir):
@@ -84,13 +82,11 @@ for current_dir in dirs:
             # Different time range than other variables
             src_file_name = f"{current_dir}_{year}_months_4_to_9.nc"
         else:
-            src_file_name = f"{current_dir.lower()}_{year}.nc"
-        
+            src_file_name = f"{current_dir.lower()}_{year}.nc"     
 
         src_path = os.path.join(src_root, current_dir, src_file_name)
         
         if not os.path.exists(src_path):
-            #print(f"path {src_path} doesn't exists")
             with open(os.path.join(dst_root, "error_log.txt"), 'a') as error_log:
                 error_log.write(f"Path does not exist:{src_path}\n")
             continue
